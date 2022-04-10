535c36f508e070d2b9347c097baf5b132a03b99e
shashankraj1987 <shashank1_theone@yahoo.co.in>
2022-04-10 23:03:54 +0530

    init file updated

--- Final/notebooks/init.py
+++ Final/notebooks/init.py
@@ -1,15 +1,16 @@
-from datetime import datetime
+# %%
 import pandas as pd
+#from datetime import datetime
 import sys
 import os
 from sqlalchemy import create_engine
-from datetime import datetime
 import shutil
 
 import check_server
 import log_config
 import organize_files as of
 
+# %%
 try:
     tmp_fold = os.environ['tmp']
 except:
@@ -18,11 +19,13 @@ else:
     log_vals = log_config.start_log(tmp_fold)
     init_logger = log_vals[0]
 
+# %%
 SCHEMA_REF = {
     "Client Billing Descending":"client_billing","Fee Breakdown by Dept and Fee Earner":"fee_brkdn_dept_fe","Fee Summary by Dept and Fee Earner":"fee_smry_dept_fe",
     "Fees Billed":"fees_billed","Matter Source of Business inc Matter Bills (Bill Date)":"mttr_src_ref","Total Hours by Fee Earner-With Billings All":"tot_hrs_by_fe",
     "Matters Opened by FE":"mtrs_by_fe","Payment Received Analysis":"pmt_rcv_analysis"}
 
+# %%
 def init_checks(info_file=None):
     all_details = {}
     info_dict = {}
@@ -97,35 +100,27 @@ def init_checks(info_file=None):
             init_logger.info("Environment Variables not set for DB Credentials.")
             return -1
 
-def get_consolidated_data_dict():
-    
+# %%
+def get_consolidated_data_dict(info_file=None):
     if os.name == 'posix':
         init_logger.info("Getting Details from the Environment Variables")
         all_info = init_checks()
     elif os.name == 'nt':
         init_logger.info("Running on Windows")
-        loc = r"D:\Learning+Offline\db_creds.csv"  ## Remove this line in Final 
-        #loc = input("Enter Location of Configuration file")
+        loc = info_file
         if os.path.exists(loc):
             all_info = init_checks(loc)
         else:
             init_logger.info(f"File [{loc}] not Found")
             sys.exit()
 
-    try:
-        engine = create_engine(f'postgresql://{all_info["db_user"]}:{all_info["db_password"]}@{all_info["db_host"]}:{all_info["db_port"]}/{all_info["db"]}',echo=True)
-    except:
-        init_logger.info("Unable to connect to the Database")
-    else:
-        init_logger.info("Database Connection Established")
-
     dict_list = of.categorize_files(all_info["file_loc"],init_logger)
     all_files = of.concat_files(dict_list, all_info["file_loc"],init_logger)
     return all_files, all_info
 
-def write_data_to_filesys():
-    
-    ret_vals = get_consolidated_data_dict()
+# %%
+def write_data_to_filesys(info_file_loc=None):
+    ret_vals = get_consolidated_data_dict(info_file_loc)
     all_files_dict = ret_vals[0]
     all_info_dict = ret_vals[1]
     final_files = all_info_dict['final_files']
@@ -165,8 +160,47 @@ def write_data_to_filesys():
                 init_logger.info(f'Creating Backup File [{fname}]')
                 tmp_df.to_parquet(fname,compression = 'gzip')
             init_logger.info("*"*50)
-    init_logger.info(f'Moving log files from [{log_vals[1]}] to [{all_info_dict["log_file"]}]')
-    shutil.copy(log_vals[1],all_info_dict["log_file"])
     return all_files_dict,all_info_dict
+
+# %%
+def write_data_to_db(file_loc=None):
+    info = write_data_to_filesys(file_loc)
+    local_all_info = info[1]
+    local_all_files = info[0]
+    try:
+        engine = create_engine(f'postgresql://{local_all_info["db_user"]}:{local_all_info["db_password"]}@{local_all_info["db_host"]}:{local_all_info["db_port"]}/{local_all_info["db"]}',echo=True)
+    except:
+        init_logger.info("Unable to connect to the Database, Exiting.. ")
+        sys.exit()
+    else:
+        init_logger.info("Database Connection Established")
+
+    final_files = local_all_info['final_files']
+
+    for file in local_all_files.keys():
+        #init_logger.debug(f"Converting all Columns to Lowercase for [{file}]")
+        local_all_files[file].columns = [cols.lower() for cols in local_all_files[file].columns]
+
+        fname = final_files+"/"+file+".csv"
+        init_logger.debug(f"Appending all data in Postgresql Server for [{file}]")
+
+        try:            
+            local_all_files[file].to_sql(SCHEMA_REF[file],con=engine,if_exists='append',index=None)   # Send the data to database
+        except:
+            init_logger.error(f"***** Unable to write to Database for file [{file}]********* ")
+        else:
+            continue
+        init_logger.info(f'Processed File [{file}]')
+    init_logger.info(f'Moving log files from [{log_vals[1]}] to [{local_all_info["log_file"]}]')
+    shutil.copy(log_vals[1],local_all_info["log_file"])
+
+# %%
+def main():
+    info_loc = "D:\Learning+Offline\db_creds.csv"
+    write_data_to_db(info_loc)   
+
+# %%
+if __name__ == "main":
+    main()
 
 
